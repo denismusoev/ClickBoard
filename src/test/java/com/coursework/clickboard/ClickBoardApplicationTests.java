1c1bac90fbdb8c02429cf283bfa273284a5df1cc
deniska <denis.musoev@yandex.ru>
2024-10-27 22:17:41 +0300

    Initial commit

---
+++ src/test/java/com/coursework/clickboard/ClickBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.clickboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
